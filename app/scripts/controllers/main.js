fc3a18f08bf2dcb604a4c8be00f4452d696fb943
Murray Carr <onekilo@gmail.com>
2014-04-26 12:12:14 +0100

    removed from scope unused items

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,4 @@
 
 angular.module('protractorAstrolabeExampleApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
   });
