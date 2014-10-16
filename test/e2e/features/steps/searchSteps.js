31ca7148431ac7ff1e9143cbbcc804e1c3dfd7d4
Murray Carr <onekilo@gmail.com>
2014-10-16 19:57:52 +0100

    fixes to get tests to replicate the regular protractor tests

--- test/e2e/features/steps/searchSteps.js
+++ test/e2e/features/steps/searchSteps.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var assert = require( 'assert' );
+
 var searchPage = require( '../../page/search.page' );
 
 var searchSteps = function() {
@@ -9,31 +11,34 @@ var searchSteps = function() {
 	  callback();
 	});
 
-	this.Then(/^it will have an empty search box$/, function (callback) {
-		// var value = searchPage.searchBox.getAttribute('value');
-	  // this.assert.equal( value, '' );
-	  callback();
+	this.Then(/^it will have an empty search box$/, function ( callback ) {
+		searchPage.searchBox.getAttribute( 'value' ).then( function( textValue ){
+			return assert.equal(textValue, '');
+		});
+		callback();
 	});
 
-	this.Then(/^I type "([^"]*)" into the search box$/, function (arg1, callback) {
-	  // searchPage.searchBox.sendKeys( arg1 );
-		// this.assert.equal( searchPage.searchBox.getAttribute('value'), arg1);
+	this.Then(/^I type "([^"]*)" into the search box$/, function ( arg1, callback ) {
+	  searchPage.searchBox.sendKeys( arg1 );
+	  searchPage.searchBox.getAttribute( 'value' ).then( function( textValue ){
+			return assert.equal(textValue, arg1 );
+		});
 	  callback();
 	});
 
 	this.Then(/^I can see repeated elements on the page$/, function (callback) {
 	  //awsomeThings are done via a findElements call which is async so you need to do a flush to interact with the values
-		// searchPage.awsomeThings.then( function ( rows ){
-		// 	this.assert.equal( rows.length , 3 );
-		// });
+		searchPage.awsomeThings.then( function ( rows ){
+			assert.equal( rows.length , 3 );
+		});
 
-		// //index 1 is the first entry
-		// //look for the name of the first element
-		// searchPage.awsomeThing( 1 ).name.getText().then( function ( textValue ) {
-		// 	return this.assert.equal( textValue , 'HTML5 Boilerplate' );
-		// });
+		// index 1 is the first entry
+		// look for the name of the first element
+		searchPage.awsomeThing( 1 ).name.getText().then( function ( textValue ) {
+			return assert.equal( textValue , 'HTML5 Boilerplate' );
+		});
 
-	  callback();
+		callback();
 	});
 
 };
--- test/e2e/features/support/world.js
+++ test/e2e/features/support/world.js
@@ -1,14 +1,8 @@
-/*global protractor,assert*/
 'use strict';
-
-var assert = require('assert');
 
 module.exports = function() {
 
   this.World = function World(callback) {
-    this.ptor = protractor.getInstance();
-    this.assert = assert;
-
     callback(); // tell Cucumber we're finished and to use 'this' as the world instance
   };
 };
